85fcce6caec034c130707c2d535d5b3bd55978a0
Manuel Lopera <114296815+LoperaUr@users.noreply.github.com>
2023-06-13 19:49:04 -0500

    optimizar imports del method main

--- src/main/java/com/example/libreria/LibreriaApplication.java
+++ src/main/java/com/example/libreria/LibreriaApplication.java
@@ -1,18 +1,8 @@
 package com.example.libreria;
 
-import com.example.libreria.entitie.Autor;
-import com.example.libreria.entitie.Categoria;
-import com.example.libreria.entitie.Editorial;
-import com.example.libreria.entitie.Ubicacion;
-import com.example.libreria.repository.AutorRepository;
-import com.example.libreria.repository.CategoriaRepository;
-import com.example.libreria.repository.EditorialRepository;
-import com.example.libreria.repository.UbicacionRepository;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
